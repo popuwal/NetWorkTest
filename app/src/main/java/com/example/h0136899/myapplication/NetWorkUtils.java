1eab5b9f23a02242cad57b22712e6b172d5299c3
H0136899 <1912668959@qq.com>
2018-05-09 18:11:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/h0136899/myapplication/NetWorkUtils.java
@@ -0,0 +1,12 @@
+package com.example.h0136899.myapplication;
+
+public class NetWorkUtils {
+    // 需要的权限：<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+    //获取系统服务ConnectivityManager
+    // 获取当前Active的网络信息getActiveNetwork
+
+
+    public static boolean isConnected(){
+
+    }
+}
